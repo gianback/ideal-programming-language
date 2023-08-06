25d645974aa305c536e02c67fd5069773e7cacd6
Gian Franco <gianfraback@gmail.com>
2023-08-06 01:00:29 -0500

    feat: add query lang context

---
+++ src/context/queryLangContext.tsx
@@ -0,0 +1,55 @@
+import { INITIAL_QUERY_STATE, Query } from "@/models";
+import { ReactNode, createContext, useContext, useState } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface QueryLangContext {
+  query: Query;
+  setQuery: (newQuery: Query) => void;
+  isLoading: boolean;
+  setIsLoading: (loading: boolean) => void;
+  firstReq: boolean;
+  setFirstReq: (loading: boolean) => void;
+}
+
+export const QueryLangContext = createContext<QueryLangContext>({
+  query: INITIAL_QUERY_STATE,
+  setQuery: () => {},
+  isLoading: false,
+  setIsLoading: () => {},
+  firstReq: true,
+  setFirstReq: () => {},
+});
+
+export const QueryLangProvider = ({ children }: Props) => {
+  const [query, setQuery] = useState<Query>(INITIAL_QUERY_STATE);
+  const [isLoading, setIsLoading] = useState(false);
+  const [firstReq, setFirstReq] = useState(true);
+
+  return (
+    <QueryLangContext.Provider
+      value={{
+        query,
+        setQuery,
+        isLoading,
+        setIsLoading,
+        firstReq,
+        setFirstReq,
+      }}
+    >
+      {children}
+    </QueryLangContext.Provider>
+  );
+};
+
+export const useQueryLangContext = () => {
+  const context = useContext(QueryLangContext);
+  if (context === undefined) {
+    throw new Error(
+      "useQueryLangContext must be used within a QueryLangProvider"
+    );
+  }
+  return context;
+};
