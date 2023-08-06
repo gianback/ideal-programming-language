849d56baba1b0eff188a89f797b6e3951e3c603d
Gian Franco <gianfraback@gmail.com>
2023-08-06 01:01:20 -0500

    refactor: change promot openai

--- src/services/openai.service.ts
+++ src/services/openai.service.ts
@@ -3,8 +3,8 @@ import { openai } from "@/config/openai";
 export const openaiService = async (sign: string) => {
   return await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `De estos lenguajes: Javascript,Go, Java, PHP, Python, Rust, C#, C++, Swift y Ruby,segun el signo ${sign} devuelve el nombre y una muy breve description maximo 25 palabras`,
-    temperature: 0.7,
-    max_tokens: 70,
+    prompt: `Segun el signo ${sign} solo dame el nombre del lenguaje de programacion ideal: Javascript,Go, Java, PHP, Python, Rust, Scala, C++, Swift, Dart, Kotlin y Ruby.`,
+    temperature: 0.8,
+    max_tokens: 80,
   });
 };
